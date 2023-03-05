b1fdd4dbbb1121c0217279a4b4cc1381e8c4546b
Ismael <darktwister46@gmail.com>
2023-03-05 20:50:34 +0100

    Cambio Mis Reservas

--- resources/views/misreservas.blade.php
+++ resources/views/misreservas.blade.php
@@ -3,25 +3,31 @@
 @endsection
 @section('content')
 
-
-    <table style="border: 1px solid black">
-        <tr>
-            <td>Fecha de reserva</td>
-            <td>Numero de personas</td>
-            <td>Accion</td>
-        </tr>
-
-        @isset($reservas)
-
-        @foreach ($reservas as $reserva)
-        <tr>
-            <td>{{ $reserva->horario->fecha}}</td>
-            <td>{{ $reserva->num_personas }}</td>
-            <td><a href="/rdelete/{{ $reserva->id }}">Eliminar</a></td>
-        </tr>
-        @endforeach
-        @endisset
-
-    </table>
+    <h1 style="margin-top: 2%; text-align: center; margin-right:5%">Tus reservas:</h1>
+    <div class="d-flex justify-content-center" style="margin-right:5%">
+        <table class="table table-bordered" style="margin-top: 3%; width: 50%">
+            <thead class="thead">
+                <tr class="table-secondary">
+                    <th>Fecha de reserva</th>
+                    <th>Numero de personas</th>
+                    <th>Accion</th>
+                </tr>
+            </thead>
+            <tbody>
+                @isset($reservas)
+                    @foreach ($reservas as $reserva)
+                        <tr>
+                            <td>{{ $reserva->horario->fecha }}</td>
+                            <td>{{ $reserva->num_personas }}</td>
+                            <td><a href="/rdelete/{{ $reserva->id }}">Eliminar</a></td>
+                        </tr>
+                    @endforeach
+                @endisset
+            </tbody>
+        </table>
+    </div>
+    <div class="alert alert-info" style="margin-top: 5%">
+        <h3 style="text-align: center; margin-top: 3%">Recuerda que siempre puedes cancelar tu reserva pulsando el botón de eliminar para cancelar esta reserva.</h2>
+    </div>
 
 @endsection
